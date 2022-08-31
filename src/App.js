75f0e73590b4ba91c14a12b77bbed5ce5edae133
janiceh94 <haesung94@gmail.com>
2022-08-30 22:18:44 -0700

    created operation button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style.css';
 import { useReducer} from 'react';
 import NumButtons from './components/NumButtons';
+import OperationButtons from './components/OperationButtons';
 
 export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
@@ -31,19 +32,19 @@ function App() {
       </div>
       <button className = 'spanTwo'>AC</button>
       <button>DEL</button>
-      <button>÷</button>
+      <OperationButtons operation = '÷' dispatch={dispatch}/>
       <NumButtons digit='7' dispatch={dispatch}/>
       <NumButtons digit='8' dispatch={dispatch}/>
       <NumButtons digit='9' dispatch={dispatch}/>
-      <button>*</button>
+      <OperationButtons operation = '*' dispatch={dispatch}/>
       <NumButtons digit='4' dispatch={dispatch}/>
       <NumButtons digit='5' dispatch={dispatch}/>
       <NumButtons digit='6' dispatch={dispatch}/>
-      <button>-</button>
+      <OperationButtons operation = '-' dispatch={dispatch}/>
       <NumButtons digit='1' dispatch={dispatch}/>
       <NumButtons digit='2' dispatch={dispatch}/>
       <NumButtons digit='3' dispatch={dispatch}/>
-      <button>+</button>
+      <OperationButtons operation = '+' dispatch={dispatch}/>
       <NumButtons digit='0' dispatch={dispatch}/>
       <NumButtons digit='.' dispatch={dispatch}/>
       <button className = 'spanTwo'>=</button>
--- src/components/NumButtons.js
+++ src/components/NumButtons.js
@@ -1,6 +1,6 @@
 import { ACTIONS } from "../App";
 
-export default function button({dispatch, digit}){
+export default function NumButtons({dispatch, digit}){
     return (
         <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}>
             {digit}
